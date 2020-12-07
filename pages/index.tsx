be38f008366a48a55ef33aae82655d0690701d2f
Ali Zahid <ali.zahid@live.com>
2020-12-07 20:24:08 +0500

    Styles updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
+
+import { Spinner } from '@simplish/components'
 
 const Home: NextPage = () => {
+  const [loading, setLoading] = useState(false)
+
   return (
     <>
       <Head>
@@ -14,7 +18,7 @@ const Home: NextPage = () => {
         <header className="flex flex-col lg:flex-row items-center text-center lg:text-left">
           <img alt="Simplish" className="w-16" src="/img/simplish.svg" />
           <div className="mt-4 lg:mt-0 lg:ml-4">
-            <h1 className="text-2xl font-semibold">Simplish</h1>
+            <h1 className="text-3xl font-semibold lowercase">Simplish</h1>
             <p className="text-sm text-gray-600 dark:text-gray-400">
               Dead simple todo lists
             </p>
@@ -23,9 +27,15 @@ const Home: NextPage = () => {
 
         <section className="mt-16">
           <Link href="/api/auth/sign-in?provider=github">
-            <a className="flex items-center bg-gray-900 hover:bg-gray-800 px-4 py-3 rounded-full">
+            <a
+              className="flex items-center bg-gray-900 hover:bg-gray-800 px-4 py-3 rounded-full"
+              onClick={() => setLoading(true)}>
               <span className="text-white font-medium">Sign in</span>
-              <img className="h-6 w-6 ml-2" src="/img/ui/github.svg" />
+              {loading ? (
+                <Spinner className="ml-2" />
+              ) : (
+                <img className="h-6 w-6 ml-2" src="/img/ui/github.svg" />
+              )}
             </a>
           </Link>
         </section>
